2aaf316d3f9bbddf293c3e11197f7149ce64a9b3
Hectornado123 <Hecalbgon1@gmail.com>
2026-03-03 12:25:44 +0100

    aaaaa

--- URP/Assets/FPS_RPMI/scripts/player/GunSystem.cs
+++ URP/Assets/FPS_RPMI/scripts/player/GunSystem.cs
@@ -1,11 +1,47 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GunSystem : MonoBehaviour
 {
+
+    #region General Variables
+
+    [Header("General References")]
+    [SerializeField] Camera fpsCam;
+    [SerializeField] Transform ShootPoint;
+    [SerializeField] LayerMask impactLayer;
+    RaycastHit hit;
+
+
+    [Header("Weapon Parameters")]
+    [SerializeField] int damage = 10;
+    [SerializeField] float range = 100;
+    [SerializeField] float spread = 0;
+    [SerializeField] float shootingColldown = 0.2f;
+    [SerializeField] float reloadTime = 1.5f;
+    [SerializeField] bool allowButtonHold = false;
+
+
+    [Header("Bullet Management")]
+    [SerializeField] int amoSize = 30;
+    [SerializeField] int bulletsPerTap = 1;
+    int bulletsLeft;
+
+    [Header("FeedBack references")]
+    [SerializeField] GameObject impactEffect;
+
+
+    [Header("Dev - Gun State Bools")]
+    [SerializeField] bool shooting;
+    [SerializeField] bool canShoot;
+    [SerializeField] bool reloading;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    #endregion
+    private void Awake()
     {
-        
+        bulletsLeft = amoSize;
+        canShoot = false;
     }
 
     // Update is called once per frame
@@ -13,4 +49,30 @@ public class GunSystem : MonoBehaviour
     {
         
     }
+
+    void Shoot()
+    {
+
+
+
+
+    }
+    #region
+
+    public void OnShoot(InputAction.CallbackContext context)
+    {
+
+
+
+    }
+
+
+    public void OnReload(InputAction.CallbackContext context)
+    {
+
+
+
+    }
+
+    #endregion
 }
